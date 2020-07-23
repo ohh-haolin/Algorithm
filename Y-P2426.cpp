3c058dbe6d8f365b2a09a6009c02b8c3c81b684d
DESKTOP-HAOLIN\Haolin <me@hlz.ink>
2020-07-23 16:49:31 +0800

    P2426

---
+++ Y-P2426.cpp
@@ -0,0 +1,45 @@
+#include <algorithm>
+#include <cstdio>
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+constexpr int MAX_N = 110;
+
+int N;
+int A[MAX_N];
+//dp[i][j] 从i删刀j的最小值
+int dp[MAX_N][MAX_N];
+
+void solve()
+{
+    cin >> N;
+    for(int i = 1; i <= N; ++i)
+    {
+        cin >> A[i];
+        dp[i][i] = A[i];
+    }
+
+    // // 枚举右端点
+    for(int j = 2; j <= N; ++j)
+        // 枚举左端点
+        for(int i = j-1; i > 0; --i)
+        {
+            //直接求得一个基础值
+            dp[i][j] = abs(A[i]-A[j]) * (j-i+1);
+            
+            //枚举断点
+            for(int k = i; k < j; ++k)
+                dp[i][j] = max(dp[i][j], dp[i][k] + dp[k+1][j]);
+        }
+
+
+    cout << dp[1][N] << '\n';
+}
+
+int main()
+{
+    solve();
+    return 0;
+}
